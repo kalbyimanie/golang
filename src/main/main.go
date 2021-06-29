acedc090c1f0ebf24e4b5d415b9baf403c93b201
kalbyimanie <aby.imanie@gmail>
2021-06-29 17:15:52 +0700

    modified golang files

--- src/main/main.go
+++ src/main/main.go
@@ -14,15 +14,21 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Welcome to the HomePage!")
 }
 
+func feedPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the FeedPage!")
+	fmt.Println("Welcome to the FeedPage!")
+}
+
 func handleRequest() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/home", homePage)
+	router.HandleFunc("/feed", feedPage)
 	// application_port := os.LookupEnv("APPLICATION_PORT")
 	log.Fatal(http.ListenAndServe(":80", router))
 	// log.Fatal(http.ListenAndServe(":"+application_port, router))
 }
 
 func main() {
-	fmt.Println("Server starting on port 80")
+	fmt.Println("Server starting on port 80....")
 	handleRequest()
 }
